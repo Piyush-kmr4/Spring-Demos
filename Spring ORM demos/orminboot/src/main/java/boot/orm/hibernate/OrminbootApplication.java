088a8fa27aad37cdfc64573e4e522b52dd4e60f4
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-04-18 04:17:35 +0530

    added files

---
+++ Spring ORM demos/orminboot/src/main/java/boot/orm/hibernate/OrminbootApplication.java
@@ -0,0 +1,13 @@
+package boot.orm.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrminbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrminbootApplication.class, args);
+	}
+
+}
---
+++ Spring ORM demos/orminboot/src/test/java/boot/orm/hibernate/OrminbootApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.orm.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrminbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
